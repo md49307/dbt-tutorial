a08289ec723199b93d5f25627ce8e3a13980a8ec
Meera Deshpande <meera.deshpande@jetblue.com>
2021-09-06 16:37:35 -0400

    dbt tests

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,14 @@
-{{ config(materialize= 'table') }}
+{{ config(materialized='table') }}
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw_dev.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
+        
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw_dev.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
+        
 ),
 
 customer_orders as (
